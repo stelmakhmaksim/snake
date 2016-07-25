74bc2c9f7ceeb28d985cb1d967dd16244dda35e8
stelmakhmaksim <stelmakh96@mail.ru>
2016-07-25 10:07:46 +0300

    new class HorizontalLine

---
+++ snake/snake/HorizontalLine.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snake
+{
+    class HorizontalLine
+    {
+        List<Point> pList;
+
+        public HorizontalLine()
+        {
+            Point p1 = new Point(4, 5, '+');
+            Point p2 = new Point(5, 5, '+');
+            Point p3 = new Point(6, 5, '+');
+
+            pList.Add(p1);
+            pList.Add(p2);
+            pList.Add(p3);
+        }
+
+        public void DrawLine()
+        {
+            foreach (Point p in pList)
+            {
+                p.Draw();
+            }
+        }
+    }
+}
